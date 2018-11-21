ec7a18612158c1f4f96795c2d56ec2c17d898f69
vikas <vrsahu29@gmail.com>
2018-11-21 06:27:56 -0800

    Read the csv file

---
+++ index.js
@@ -0,0 +1,16 @@
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                console.log(allText);
+
+              
+            }
+        }
+    }
+    rawFile.send(null);
+}
+readTextFile("matches.csv");
