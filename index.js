b82996ae2407f02c938ea77ae50bd7836904a9af
vikas <vrsahu29@gmail.com>
2018-11-21 06:53:09 -0800

    Listed the object property

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var matches_json = {};
+var matches_colname = [];
 function readTextFile(file) {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, true);
@@ -5,7 +7,22 @@ function readTextFile(file) {
         if (rawFile.readyState === 4) {
             if (rawFile.status === 200 || rawFile.status == 0) {
                 var allText = rawFile.responseText;
-                console.log(allText);
+                
+                var colname = "";
+                var i = 0;
+                for (i = 0; i < allText.length && allText[i] != "\n"; i++) {
+                    if (allText[i] == ",") {
+                        matches_colname.push(colname);
+                        matches_json[colname] = [];
+                        colname = "";
+                    } else if (allText[i] == " ") {
+                        colname += allText[i];
+                    }
+                    else {
+                        colname += allText[i];
+                    }
+                }
+                
 
               
             }
